220e92a648f75d6ac0b86235f4234894f8061004
ahhh <ahhh.db@gmail.com>
2014-09-28 15:39:11 -0700

    Delete rot_alpha.py

--- rot_alpha.py
+++
@@ -1,9 +0,0 @@
-from string import uppercase, lowercase, maketrans
-
-def rot_alpha(data, rot):
-
-	upper = ''.join([uppercase[(i+rot)%26] for i in xrange(26)])
-	lower = ''.join([lowercase[(i+rot)%26] for i in xrange(26)])
-	table = maketrans(uppercase + lowercase, upper + lower)
-
-	return data.translate(table)
