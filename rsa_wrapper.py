7fecf80f493e6058601f7a6cb7d60667817d84ab
ahhh <ahhh.db@gmail.com>
2015-03-01 23:46:23 -0800

    Update rsa_wrapper.py

--- rsa_wrapper.py
+++ rsa_wrapper.py
@@ -6,12 +6,13 @@ class rsa_wrapper():
     key = RSA.generate(2048, e=65537) 
     public = key.publickey().exportKey("PEM") 
     private = key.exportKey("PEM") 
-    print public
-    print private
+    return public, private
 
 if __name__ == '__main__':
   try:
     rsa = rsa_wrapper()
-    rsa.keygen()
+    public, private = rsa.keygen()
+    print public
+    print private
   except:
     print "error"
