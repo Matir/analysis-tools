502a83da81600dd69685be8b9351471d559f0c55
ahhh <ahhh.db@gmail.com>
2015-03-02 00:16:06 -0800

    Update rsa_wrapper.py
    Added functions for both encryption and decryption. Default is a test case which uses all exiting functionality with a test string

--- rsa_wrapper.py
+++ rsa_wrapper.py
@@ -1,4 +1,9 @@
+import sys
+import zlib
+import base64
 from Crypto.PublicKey import RSA 
+from Crypto.Cipher import PKCS1_OAEP
+
 
 class rsa_wrapper():
   
@@ -8,11 +13,44 @@ class rsa_wrapper():
     private = key.exportKey("PEM") 
     return public, private
 
+  def encrypt(self, public, plaintext):
+    rsakey = RSA.importKey(public) 
+    rsakey = PKCS1_OAEP.new(rsakey) 
+    offset = 0
+    encrypted = ""
+    plaintext = zlib.compress(plaintext)
+    while offset < len(plaintext):
+      encrypted += rsakey.encrypt(plaintext[offset:offset+256])
+      offset += 256
+    encrypted = base64.b64encode(encrypted)
+    return encrypted
+
+  def decrypt(self, private, ciphertext):
+    rsakey = RSA.importKey(private)
+    rsakey = PKCS1_OAEP.new(rsakey)
+    offset = 0
+    decrypted = ""
+    ciphertext = base64.b64decode(ciphertext)
+    while offset < len(ciphertext):
+      decrypted += rsakey.decrypt(ciphertext[offset:offset+256])
+      offset += 256
+    decrypted = zlib.decompress(decrypted)
+    return decrypted
+
+
 if __name__ == '__main__':
   try:
+    message = sys.argv[1]
     rsa = rsa_wrapper()
     public, private = rsa.keygen()
     print public
     print private
-  except:
-    print "error"
+
+    encrypted_message = rsa.encrypt(public, message)
+    print encrypted_message
+
+    decrypted_message = rsa.decrypt(private, encrypted_message)
+    print decrypted_message
+
+  except IndexError:
+    print "python rsa_wrapper.py <Test message to encrypt>"
